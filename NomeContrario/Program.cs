7dad948071ca1d9b2bffbec2a8c12e1116e4b9b6
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-19 18:27:55 -0300

    Novo

---
+++ NomeContrario/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomeContrario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string nomeInvertido;
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine().ToUpper();
+
+            nomeInvertido = ReverterNome(nome);
+
+            Console.Clear();
+
+            Console.WriteLine("Seu nome: " + nomeInvertido);
+
+            Console.ReadLine();
+        }
+
+        public static string ReverterNome(string nome)
+        {
+            char[] nomeinserido = nome.ToCharArray();
+            Array.Reverse(nomeinserido);
+            return new string(nomeinserido);
+        }
+    }
+}
